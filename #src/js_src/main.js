4e1510f110bbd65d53b1388e06dfd94a1d7ba820
Sergio <badzega@gmail.com>
2023-02-23 20:55:36 +0100

    ADD JS code

--- #src/js_src/main.js
+++ #src/js_src/main.js
@@ -1,1 +1,52 @@
+"use strict";
+
+
 //@prepros-append someScript.js
+
+// wyznaczamy czy to jest urządzenie mobilne i dodajemy odpowiedni klas
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        document.body.classList.add("_touches");
+} else {
+    document.body.classList.add("_pc");
+};
+
+
+// burger menu
+const menuBody = document.querySelector(".menu_body");
+const iconMenu = document.querySelector(".menu_icon");
+if (iconMenu) {
+    
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
+
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const goToBlock = document.querySelector(menuLink.dataset.goto);
+            const goToBlockValue = goToBlock.getBoundingClientRect().top + pageYOffset - document.querySelector(".header").offsetHeight;
+
+            if(iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+
+            }
+            window.scrollTo({
+                top: goToBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
